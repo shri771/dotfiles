1eca1260cf2675f1379ff8e328f6c1f2ed79e9fd
Shrikant Shingare <shrikantshingare77@gmail.com>
2024-12-25 22:36:17 +0530

    test

--- .config/microsoft-edge-beta/Default/Extensions/ndcileolkflehcjpmjnfbnaibdcgglog/6.11.1_0/adblock-betafish/ipm/background/data-collection.d.ts
+++
@@ -1,4 +0,0 @@
-import { PayloadData } from "./data-collection.types";
-export declare function getPayload(): Promise<PayloadData>;
-export declare function clearEvents(): Promise<void>;
-export declare function storeEvent(ipmId: string, commandName: string, commandVersion: number, name: string): Promise<void>;
