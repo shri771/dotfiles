a3a218d756d81fb3c0cf0f289019c2ec3858d55c
Shrikant Shingare <shrikantshingare77@gmail.com>
2025-01-31 22:17:32 +0530

    updated

--- fish/config.fish
+++ fish/config.fish
@@ -395,9 +395,9 @@ end
 ### ALIASES ###
 # navigation
 alias .='cd'
-alias ...='cd ../..'
 alias ..='cd ..'
 alias .3='cd ../../..'
+alias .2='cd ../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
