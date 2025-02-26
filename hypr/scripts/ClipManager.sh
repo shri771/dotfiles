5739513626fdcb7472a7ebb7067ec4d8a6f6c085
Shrikant Shingare <shrikantshingare77@gmail.com>
2025-02-26 08:58:27 +0530

    Update config.fish Update ClipManager.sh Update .wallpaper_current Update .current_wallpaper Update Modules

--- hypr/scripts/ClipManager.sh
+++ hypr/scripts/ClipManager.sh
@@ -2,22 +2,25 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # Clipboard Manager. This script uses cliphist, rofi, and wl-copy.
 
+# Variables
+rofi_theme="$HOME/.config/rofi/config-clipboard.rasi"
+msg='👀 **note**  CTRL DEL = cliphist del (entry)   or   ALT DEL - cliphist wipe (all)'
 # Actions:
 # CTRL Del to delete an entry
 # ALT Del to wipe clipboard contents
-# RETURN to paste the selected entry
 
-# Check if rofi is already running and kill it if necessary
+# Check if rofi is already running
 if pidof rofi > /dev/null; then
-    pkill rofi
+  pkill rofi
 fi
 
 while true; do
     result=$(
-        cliphist list | rofi -i -dmenu \
+        rofi -i -dmenu \
             -kb-custom-1 "Control-Delete" \
             -kb-custom-2 "Alt-Delete" \
-            -config ~/.config/rofi/config-clipboard.rasi
+            -config $rofi_theme < <(cliphist list) \
+			-mesg "$msg" 
     )
 
     case "$?" in
@@ -25,12 +28,15 @@ while true; do
             exit
             ;;
         0)
-            if [[ -n "$result" ]]; then
-                cliphist decode <<<"$result" | wl-copy
-                sleep 0.1  # Allow time for clipboard update
-                wl-paste | wl-copy
-                exit
-            fi
+            case "$result" in
+                "")
+                    continue
+                    ;;
+                *)
+                    cliphist decode <<<"$result" | wl-copy
+                    exit
+                    ;;
+            esac
             ;;
         10)
             cliphist delete <<<"$result"
