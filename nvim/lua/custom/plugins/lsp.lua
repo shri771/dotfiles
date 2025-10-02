7717c906cf6103eadc107206c66ef4c6e90abb61
Shrikant Shingare <shrikantshingare77@gmail.com>
2025-10-02 12:45:34 +0530

    Resloved the Lsp issue

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -6,7 +6,6 @@ return {
     "ray-x/go.nvim",
     dependencies = {
       "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
