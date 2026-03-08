8bc5c6e29818c2e0793c0e25f697b69d3a859efd
Shrikant Shingare <shrikantshingare77@gmail.com>
2026-03-08 21:43:59 +0530

    Removed kde
    Signed-off-by: Shrikant Shingare <shrikantshingare77@gmail.com>

--- nix-config/nixos/configuration.nix
+++ nix-config/nixos/configuration.nix
@@ -167,7 +167,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = false;
 
   # WireShrak
   programs.wireshark.enable = true;
