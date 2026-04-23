6088f89919e0a75d0fa306c525102f9c3788f690
Shrikant Shingare <shrikantshingare77@gmail.com>
2026-04-23 21:05:22 +0530

    Removed linkwarden
    Signed-off-by: Shrikant Shingare <shrikantshingare77@gmail.com>

--- nix-config/nixos/configuration.nix
+++ nix-config/nixos/configuration.nix
@@ -379,7 +379,7 @@ in
 
   # Docker-Container
   services.my-vaultwarden.enable = true;
-  services.my-linkwarden.enable = true;
+  # services.my-linkwarden.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.11";
